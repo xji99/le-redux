8c373a814094349b473fa98af55132ab79c99805
xji999 <xji999@gmail.com>
2018-08-27 01:37:16 -0400

    install axios and ajax call in action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,18 @@
-const API_KEY='e22d54f5710dd63dcc9f9a31cb3b392b';
+import Axios from "axios";
 
+const API_KEY='e22d54f5710dd63dcc9f9a31cb3b392b';
+const ROOT_URL  = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'Fetch_weather';
+
+//action creator return action
+export function fetchWeather(city){
+
+    const url = `${ROOT_URL}&q=${city},us`;
+
+    return  {
+        type : FETCH_WEATHER,
+        payload: Axios.get(url)
+    }
+
+}
