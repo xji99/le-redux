954e53998930aa4731f52777c181784efde97b0e
xji999 <xji999@gmail.com>
2018-08-27 02:18:53 -0400

    render chart with sparkline

--- src/container/weather_list.js
+++ src/container/weather_list.js
@@ -1,9 +1,25 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-
+import {Sparklines, SparklinesLine} from 'react-sparklines';
 
 class WeatherList extends Component{
 
+
+    renderWeather = (cityData)=>{
+        const temps = cityData.list.map(weather=>weather.main.temp);
+
+        return(
+            <tr key={cityData.city.name}>
+                <td>{cityData.city.name}</td>
+                <td>
+                    <Sparklines height={120} width={180} data={temps}>
+                        <SparklinesLine color="red"/>
+                    </Sparklines>
+
+                </td>
+            </tr>
+        );
+    }
 
     render(){
         return(
@@ -17,6 +33,7 @@ class WeatherList extends Component{
                     </tr>    
                 </thead>
                 <tbody>
+                    {this.props.weather.map(this.renderWeather)}
                 </tbody>
             </table>
 
